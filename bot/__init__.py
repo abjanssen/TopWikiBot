72187f6353103ece19924e69a88b0b52a0d6e93b
abjanssen <154879838+abjanssen@users.noreply.github.com>
2023-12-27 14:01:49 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,8 +2,7 @@ import nltk
 import os
 
 from .wikibot import main
-from . import post
-from . import creds
+from . import main
 
 if not os.path.exists("nltk_data"):
     nltk.download("punkt", download_dir="nltk_data/")
