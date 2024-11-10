6801e5b15e3ce4b934ca62b4f2bff80773bc60a0
abjanssen <154879838+abjanssen@users.noreply.github.com>
2024-11-10 14:01:25 +0100

    update

--- featarticle_bot/featarticle_bot.py
+++ featarticle_bot/featarticle_bot.py
@@ -265,7 +265,8 @@ def create_post(text: str):
         if facets:
             post["facets"] = facets
 
-    wikipedia_url=get_wikipedia_data()[1]
+    wikipedia_data=get_wikipedia_data()
+    wikipedia_url = wikipedia_data[1]
     url = fix_url_format(wikipedia_url)
     if len(url) > 0: 
          url_card = fetch_embed_url_card()
