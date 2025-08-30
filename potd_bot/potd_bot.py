cf69f0155d4a9e589a3e2406657dcbfd34df8931
abjanssen <154879838+abjanssen@users.noreply.github.com>
2025-08-30 16:01:50 +0200

    u

--- potd_bot/potd_bot.py
+++ potd_bot/potd_bot.py
@@ -25,7 +25,7 @@ def bsky_login_session(handle: str, password: str) -> Dict:
 
 #Define function to get the date in the printable form needed
 def date_of_interest():
-    today = date.today() - timedelta(21)
+    today = date.today() - timedelta(35)
     year = today.strftime("%Y")
     month = today.strftime("%m")
     day = today.strftime("%d")
@@ -112,7 +112,7 @@ def get_wikipedia_data():
             print(2)
             #trim_file = ffmpeg.trim(input_file, start = 0, end = 10)
             #print(3)
-            output_file = ffmpeg.output(input_file, "feat_video_adapt.webm", c="copy")
+            output_file = ffmpeg.output(input_file, "feat_video_adapt.webm")
             print(4)
             output_file.run(overwrite_output=True)
             print(5)
